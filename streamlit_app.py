44f81f74bcd324e5e7f1ff60a73d1c7d33115dbe
AkkuPawar27 <146732545+AkkuPawar27@users.noreply.github.com>
2023-10-28 13:26:29 +0530

    Update streamlit_app.py
    change index to fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,5 +9,8 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.multiselect("Pick some Fruits:",list(my_fruit_list.index))
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
